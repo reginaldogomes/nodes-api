846bff04345663c1a5629a6052d28a98ceab308e
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2020-04-18 16:29:45 -0300

    mongodb and mongoose configured, model tutorial created

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+const db = require('./app/models')
+
 const app = express()
 
 const corsOptions = {
     origin: 'http://localhost/8081'
 }
 
+app.use(cors(corsOptions))
+
 app.use(bodyParser.json())
 
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // simple route
 app.get('/', (req, res) => {
